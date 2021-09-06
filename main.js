8a503482b31852a67e74f45aec8446b28c420cab
tuwokHowardii <tuwokhowardii3@gmail.com>
2021-09-06 20:15:27 +1000

    Adding electron boilerplate

---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron')
+const url = require('url')
+const path = require('path')
+
+let win 
+
+function createWindow() {
+     win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nativeWindowOpen: true,
+            preload: path.join( __dirname, '/src/js/preload.js')
+        }
+    })
+
+    win.loadFile('index.html')
+
+    win.on('closed', () => {
+        win = null
+    })
+
+}
+
+app.whenReady().then( () => {
+    createWindow()
+
+    app.on('activate', function() {
+        if(BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', function() {
+    if(process.platform !== 'darwin'){
+        app.quit()
+    }
+})
